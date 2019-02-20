d4bf6268a0addacbf1bc06640c4b2a16c5484f7a
dev65 <o.tielkov@gmail.com>
2019-02-20 18:13:52 +0200

    php manager in progress

--- php.py
+++ php.py
@@ -1,22 +1,53 @@
 import os
 import subprocess
 
+class PHP(object):
+
+    positiveRespone = ["yes", "y", "ok"]
+    negativeRespone = ["no", "n", "nok"]
+    cmdCurrent = ["sudo", "-S", "php", "-v"]
+    phpVersions = ["5.6", "7.0", "7.1", "7.2", "7.3"]
+
+    def getNewVersion(self):
+        wantedVersion = str(input("Write php version: "))
+        if wantedVersion in self.phpVersions:
+            return wantedVersion
+        else:
+            print("Check avaliavle version please")
+            self.getNewVersion()
+
+    def getCurrentVersion(self):
+        phpC = subprocess.run(self.cmdCurrent, stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+        phpCList = phpC.split()
+
+        if phpCList[0].strip() == "PHP":
+            return phpCList[1].strip()[:3]  # Cut whitespaces from both sides and cut first 3 sybols #
+        else:
+            print('Something went wrong')
+
+    def setNewVersion(self):
+        print('new version change')
+
+    def initPhpVersion(self):
+        process = input("Your current php version: " + self.getCurrentVersion() + ". Continue? [y/n]: ")
+        if self.getProcess(process):
+            self.setNewVersion()
+        else:
+            return False
 
 
-# os.system("sudo -S php -v")
+    def getProcess(self, response):
+        if response in self.positiveRespone:
+            return True
+        elif response in self.negativeRespone:
+            return False
+        else:
+            self.initPhpVersion()
 
-# subprocess.call(["sudo", "-S", "php", "-v"])
-# subprocess.call(["sudo", "-S", "php", "-i"])
-p = subprocess.run(["sudo", "-S", "php", "-i"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+php = PHP()
+newVersion = php.initPhpVersion()
 
-# f = open("php.txt","w+")
-# f.write(p.stdout.decode('utf-8'))
-# f.close()
 
-s = str.find(p, "PHP Version")
-
-p = p[s:s+50]
-
-print(p)
 
 
